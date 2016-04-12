c2f4199ea50aa8ff2e9b3900a029348f4690201e
mrtakata <matheustakata@Matheuss-MacBook-Pro.local>
2016-04-12 11:11:33 -0300

    closes #1, calculate BMI based on 2 inputs

---
+++ BMI/app/src/main/java/br/ufscar/mrtakata/bmi/DisplayMessageActivity.java
@@ -0,0 +1,33 @@
+package br.ufscar.mrtakata.bmi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
+    }
+}
--- BMI/app/src/main/java/br/ufscar/mrtakata/bmi/MainActivity.java
+++ BMI/app/src/main/java/br/ufscar/mrtakata/bmi/MainActivity.java
@@ -1,13 +1,32 @@
 package br.ufscar.mrtakata.bmi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "br.ufscar.mrtakata.bmi.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText height_value = (EditText) findViewById(R.id.height_value);
+        EditText weight_value = (EditText) findViewById(R.id.weight_value);
+        float height = Float.valueOf(height_value.getText().toString());
+        float weight = Float.valueOf(weight_value.getText().toString());
+        String message = Float.toString(calculateBMI(height, weight));
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    private float calculateBMI(float height, float weight){
+        return weight / (height * height);
     }
 }
